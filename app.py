f52ac8bb45190e383a55c62829cad0c8bf3c0c09
mohammadakashahammed <mohammadakashahammed@gmail.com>
2023-11-26 03:31:29 +0600

    Rendering a Template and Using Variables

--- app.py
+++ app.py
@@ -1,36 +1,12 @@
-from markupsafe import escape
-from flask import Flask, abort
+import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
-
-
-@app.route('/about/')
-def about():
-    return '<h3>This is a Flask web application.</h3>'
-
-
-@app.route('/capitalize/<word>/')
-def capitalize(word):
-    return '<h1>{}</h1>'.format(escape(word.capitalize()))
-
-
-@app.route('/add/<int:n1>/<int:n2>/')
-def add(n1, n2):
-    return '<h1>{}</h1>'.format(n1 + n2)
-
-
-@app.route('/users/<int:user_id>/')
-def greet_user(user_id):
-    users = ['Akash Ahammed', 'Numan', 'Shuvo Khan', 'Sifat Ahammed']
-    try:
-        return '<h2>Hi {}</h2>'.format(users[user_id])
-    except IndexError:
-        abort(404)
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 
 if __name__ == "__main__":
