b2ff39d253d3989e6f12994a064d5d604f676bc5
mohammadakashahammed <mohammadakashahammed@gmail.com>
2023-11-26 02:45:55 +0600

    Routes and View Functions

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ def hello():
     return '<h1>Hello, World!</h1>'
 
 
+@app.route('/about/')
+def about():
+    return '<h3>This is a Flask web application.</h3>'
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
