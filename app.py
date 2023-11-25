39eb0dee7166d38b5d23cbf81f1e80a081fb131b
mohammadakashahammed <mohammadakashahammed@gmail.com>
2023-11-26 02:52:08 +0600

    Dynamic Routes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from markupsafe import escape
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,6 +12,16 @@ def hello():
 @app.route('/about/')
 def about():
     return '<h3>This is a Flask web application.</h3>'
+
+
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+
+
+@app.route('/add/<int:n1>/<int:n2>/')
+def add(n1, n2):
+    return '<h1>{}</h1>'.format(n1 + n2)
 
 
 if __name__ == "__main__":
