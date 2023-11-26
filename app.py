ff3684e1b174120b63b53627239e879c37dc0135
mohammadakashahammed <mohammadakashahammed@gmail.com>
2023-11-26 22:39:53 +0600

    Using Conditionals and Loops

--- app.py
+++ app.py
@@ -15,6 +15,17 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/comments')
+def comments():
+    comments = ['This is the first comment.',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.'
+                ]
+
+    return render_template('comments.html', comments=comments)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
